b68e87fb5ab13551fbca8fc9c50484016aeaa281
IlyaGrevtsov <ilyagrevtsov00@gmail.com>
2023-12-20 14:37:38 +0300

    Минимальная поддерживаемая IOS 13 Отключена сборка на IPad Изменил цвет фона в LaunchScreen в соответствии с макетом Убрал выделение ячейки Изменил BackGround в StoryBoard в соответствии с макетом Изменил размер шрифта в Data Label Изменил расположение LikeButton

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
-static let reuseIdentifier = "ImagesListCell"
+    static let reuseIdentifier = "ImagesListCell"
     
     @IBOutlet weak var dateLabel: UILabel!
     
