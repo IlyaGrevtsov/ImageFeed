a9132e5fb03a0d0a0279e952911a1f8af2b95790
IlyaGrevtsov <145673061+IlyaGrevtsov@users.noreply.github.com>
2024-03-19 11:51:07 +0300

    Revert "Sprint 11"

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -1,15 +1,7 @@
 import Foundation
 
-let AccessKey = "jpUG2ubiqkre46Xu1fTcDNkh2HsnBWv2BwgsqqsSnLM"
-let SecretKey = "b0k4PmJnAL1K7QGWSUTgvMr1lkS7KSpkzcaF8sRL-HE"
-let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessKey = "<jpUG2ubiqkre46Xu1fTcDNkh2HsnBWv2BwgsqqsSnLM>"
+let SecretKey = "<b0k4PmJnAL1K7QGWSUTgvMr1lkS7KSpkzcaF8sRL-HE>"
+let RedirectURI = "<urn:ietf:wg:oauth:2.0:oob>"
 let AccessScope = "public+read_user+write_likes"
 let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
-
-public enum Constants {
-    static let defaultApiBaseURLString = "https://api.unsplash.com"
-    static let secretKey: String = "b0k4PmJnAL1K7QGWSUTgvMr1lkS7KSpkzcaF8sRL-HE"
-    static let profileRequestPathString = "/me"
-    static let getMethodString = "GET"
-    static let bearerToken = "bearerToken"
-}
