a68b2e841aadc6b6ecb83b2d0b27fecb841b739b
IlyaGrevtsov <ilyagrevtsov00@gmail.com>
2024-03-19 11:26:56 +0300

    Данные профиля обновляются.

---
+++ ImageFeed/ProfileService/ProfileStructures.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+struct ProfileResult: Codable {
+    let userName: String
+    let firstName: String?
+    let lastName: String?
+    let bio: String?
+    let profileImage: ProfileImage?
+    
+    private enum CodingKeys: String, CodingKey {
+        case userName = "username"
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case bio
+        case profileImage = "profile_image"
+    }
+}
+
+struct Profile {
+  let username: String
+  let name: String
+  let loginName: String
+  let bio: String?
+}
+
+struct ProfileImage: Codable {
+  let small: String?
+  let medium: String?
+  let large: String?
+}
+
+
+
+
+// MARK: - Init for ProfileResult
+
+extension Profile {
+
+  init(result profile: ProfileResult) {
+    self.init(
+      username: profile.userName,
+      name: "\(profile.firstName ?? "") \(profile.lastName ?? "")",
+      loginName: "@\(profile.userName)",
+      bio: profile.bio
+    )
+  }
+}
